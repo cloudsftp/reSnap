980a769aa108c2abab1468d45b81537594184ec1
Fabian Weik <fabian.weik@protonmail.com>
2020-11-20 17:24:46 +0100

    init

---
+++ reSnap.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+ssh_host="root@10.11.99.1"
+output_file=snapshot.png
+
+# technical parameters
+width=1408
+height=1872
+bytes_per_pixel=2
+loop_wait="true"
+loglevel="info"
+
+ssh_cmd() {
+  ssh -o ConnectTimeout=1 "$ssh_host" "$@"
+}
+
+# check if we are able to reach the remarkable
+if ! ssh_cmd true; then
+    echo "$ssh_host unreachable"
+    exit 1
+fi
+
+# compression commands
+compress="\$HOME/lz4"
+decompress="lz4 -d"
+
+# calculate how much bytes the window is
+window_bytes="$((width * height * bytes_per_pixel))"
+
+# read the first $window_bytes of the framebuffer
+head_fb0="dd if=/dev/fb0 count=1 bs=$window_bytes 2>/dev/null"
+
+read_command="$head_fb0 | $compress"
+
+ssh_cmd "$read_command" \
+  | $decompress \
+  | ffmpeg -y \
+    -f rawvideo \
+    -pixel_format rgb565le \
+    -video_size "$width,$height" \
+    -i - \
+    -frames:v 1 $output_file
+
+feh $output_file
